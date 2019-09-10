72100bb3ef2a6ea051b970787bd6f679c10d1a52
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-09-10 18:07:23 -0300

    fsdffdsfdfsfd

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bd6b0b9ac25467ab961b84c823c272a.js"
+  "/jeancarlos-cpu/starwars/precache-manifest.f1a969c4029162e725afd887de8a06ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jeancarlos-cpu/starwars/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
