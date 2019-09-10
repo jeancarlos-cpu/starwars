462bbdb66311d0b7a0b6e81d47c2b741c138654e
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-09-10 17:34:01 -0300

    init

---
+++ precache-manifest.0bd6b0b9ac25467ab961b84c823c272a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de88e8d282554624952dd78d33ad7e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de02afadca118568a861",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7e519adb2e91fbd824f7",
+    "url": "/static/css/main.b5772444.chunk.css"
+  },
+  {
+    "revision": "de02afadca118568a861",
+    "url": "/static/js/2.7f238ee9.chunk.js"
+  },
+  {
+    "revision": "7e519adb2e91fbd824f7",
+    "url": "/static/js/main.b51bf249.chunk.js"
+  },
+  {
+    "revision": "e1eca716ea603bf4024b",
+    "url": "/static/js/runtime~main.1fefafac.js"
+  },
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/static/media/Starjhol.9c91297e.ttf"
+  },
+  {
+    "revision": "f72f509a2014f09c1ebf1c81294ed792",
+    "url": "/static/media/Starjout.f72f509a.ttf"
+  }
+]);
