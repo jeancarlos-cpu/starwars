a93c448f6168793eb5485e3eb022dea5eac8ba33
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-09-11 00:47:10 -0300

    Updates

---
+++ precache-manifest.9e539696256fd21e7ce5f924d62f517b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06361c434e2aee06d236b34b34bdea0",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "de02afadca118568a861",
+    "url": "/starwars/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d95208b20e79591c798a",
+    "url": "/starwars/static/css/main.7aff239a.chunk.css"
+  },
+  {
+    "revision": "de02afadca118568a861",
+    "url": "/starwars/static/js/2.7f238ee9.chunk.js"
+  },
+  {
+    "revision": "d95208b20e79591c798a",
+    "url": "/starwars/static/js/main.8b10da9f.chunk.js"
+  },
+  {
+    "revision": "9e17b89cb66cb757e319",
+    "url": "/starwars/static/js/runtime~main.e8e04b7d.js"
+  },
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/starwars/static/media/Starjhol.9c91297e.ttf"
+  },
+  {
+    "revision": "f72f509a2014f09c1ebf1c81294ed792",
+    "url": "/starwars/static/media/Starjout.f72f509a.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b0e3326d1bb798461cdda0e76d4b0f44.js"
+  "/starwars/precache-manifest.9e539696256fd21e7ce5f924d62f517b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwars/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
